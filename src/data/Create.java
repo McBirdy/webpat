4162ca5908f864e0166851f8b85421b4ad4e8147
rudzter <rudismudi@gmail.com>
2015-05-28 10:53:50 +0200

    Test commit for å merge

--- src/data/Create.java
+++ src/data/Create.java
@@ -1,6 +1,8 @@
 package data;
 import java.util.ArrayList;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
 import model.Recording;
 import model.Site;
 
@@ -8,17 +10,23 @@ public class Create {
 	
 	
 	public static void main(String[] args) {
-		ArrayList<Site> list = new ArrayList<Site>();
 		
-		list.add(new Site());
-		list.add(new Site("Magero", "158.112.111.11", "Testing Magero"));
-		list.add(new Site("Soreisa", "12.2.2.3", "Testing Soreisa"));
-		list.get(0).recordings.add(new Recording()); 
-		System.out.println(list.get(0).recordings.get(0));
+		ArrayList<Site> siteList = new ArrayList<Site>();
+		
+		siteList.add(new Site());
+		siteList.add(new Site("Magero", "158.112.111.11", "Testing Magero"));
+		siteList.add(new Site("Soreisa", "12.2.2.3", "Testing Soreisa"));
+		siteList.get(1).recordings.add(new Recording()); 
+		siteList.get(2).recordings.add(new Recording());
+		System.out.println(siteList.get(0).recordings.get(0));
+		System.out.println(siteList.get(0).recordings.size());
+		//gets the last recording
+		//siteList.get(0).recordings.get(siteList.get(0).recordings.size());
 		
 		
-		for (Site site : list) {
+		for (Site site : siteList) {
 			System.out.printf("site: %s comment: %s\n", site.location, site.comment);
+			System.out.println(site.recordings.get(site.recordings.size() - 1));
 		}
 	}
 }
